f0574141b7cf40b56e608087cda89a576c3ab3bf
Jinxuan Cui <i@cjxol.com>
2018-05-11 23:58:04 +0100

    Website base

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='website'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('events/bbq/', views.events_bbq, name='events_bbq')
 ]
