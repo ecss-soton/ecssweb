b4ac72905e0954a6adbf63df1dad2a07a5227ce6
Jinxuan Cui <i@cjxol.com>
2018-05-26 16:45:20 +0100

    Change footer style, add homepage

--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'website/index.html')
+def home(request):
+    return render(request, 'website/home.html')
 
 def events_bbq(request):
     return render(request, 'website/events/bbq.html')
