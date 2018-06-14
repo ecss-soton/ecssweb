87235e98471e835a9cb91fb464e4cd07a0e930f4
Jinxuan Cui <i@cjxol.com>
2018-06-14 14:23:21 +0100

    Changes to portal

--- portal/views.py
+++ portal/views.py
@@ -4,15 +4,3 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def overview(request):
     return render(request, 'portal/overview.html')
-
-@login_required
-def feedback_submit(request):
-    pass
-
-@login_required
-def feedback_view(request):
-    pass
-
-@login_required
-def feedback_respond(request):
-    pass
