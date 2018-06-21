5d34d33390072bb9172f97411a96e4ad13640ef3
Jinxuan Cui <i@cjxol.com>
2018-06-22 00:40:19 +0100

    Add tests for _get_page_range

--- feedback/tests.py
+++ feedback/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from .models import Feedback, Category, Response
 
+from .views import _get_page_range
+
 class FeedbackTestCase(TestCase):
 
     def setUp(self):
@@ -10,6 +12,7 @@ class FeedbackTestCase(TestCase):
         category_others.save()
 
     def test_feedback(self):
+        # Get Others category
         category_others = Category.objects.get(pk='Others')
 
         # Test create one feedback
@@ -63,3 +66,24 @@ class FeedbackTestCase(TestCase):
         feedback01.save()
         feedback01.delete()
         self.assertEqual(Category.objects.get(pk='Website'), category_website)
+
+class PaginatorTestCase(TestCase):
+
+    def test_get_page_range(self):
+        # Test enough space to show all pages
+        self.assertEqual(_get_page_range(1, 10, 5), range(1, 11))
+
+        # Test current page in the middle
+        self.assertEqual(_get_page_range(4, 10, 2), range(2, 7))
+
+        # Test current page in the front
+        self.assertEqual(_get_page_range(2, 10, 2), range(1, 6))
+
+        # Test current page in the back
+        self.assertEqual(_get_page_range(8, 10, 3), range(4, 11))
+
+        # Test current page is the first one
+        self.assertEqual(_get_page_range(1, 10, 3), range(1, 8))
+
+        # Test current page is the last one
+        self.assertEqual(_get_page_range(10, 10, 3), range(4, 11))
