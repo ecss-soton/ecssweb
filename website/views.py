a38213d3b9094536ba77e62d03c87bb9cc7f6e33
Jinxuan Cui <i@cjxol.com>
2018-08-08 18:20:58 +0100

    New welfare page Need more style thing

--- website/views.py
+++ website/views.py
@@ -1,22 +1,32 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response
 
+
 def home(request):
     return render(request, 'website/home.html')
+
 
 def events(request):
     return render(request, 'website/events/events.html')
 
+
+def welfare(request):
+    return render(request, 'website/welfare.html')
+
+
 def media_notice(request):
     return render(request, 'website/media-notice.html')
+
 
 # 404
 def page_not_found(request, exception):
     return render(request, 'website/error_pages/404.html', status=404)
 
+
 # 403
 def permission_denied(request, exception):
     return render(request, 'website/error_pages/403.html', status=403)
+
 
 # 500
 def server_error(request):
