4f63b5266ca7949c5e35dbd160825491a20d043c
Jinxuan Cui <i@cjxol.com>
2018-09-12 16:55:31 +0100

    Tests for user/groups

--- ecsswebauth/tests.py
+++ ecsswebauth/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase
 
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+
+from ecsswebauth.models import EcsswebUserGroup, SamlUser
 
 class AuthTestCase(TestCase):
 
@@ -31,3 +33,36 @@ class AuthTestCase(TestCase):
         # Test update userinfo while authenticate the user
         user01 = authenticate(request=None, username='test01', groups=[], email='test01_new_email@example.com', givenname='givenname01', surname='surname01')
         self.assertEqual(user01.email, 'test01_new_email@example.com')
+
+
+class UserTestCase(TestCase):
+
+    def test_ecsswebusergroup_str(self):
+        group = Group.objects.create(name='test_group')
+        ecsswebusergroup = EcsswebUserGroup(group=group, is_saml=False)
+        self.assertEqual(str(ecsswebusergroup), 'test_group')
+
+    def test_ecsswebusergroup_natural_key(self):
+        group = Group.objects.create(name='test_group')
+        ecsswebusergroup = EcsswebUserGroup(group=group, is_saml=False)
+        self.assertEqual(ecsswebusergroup.natural_key(), ('test_group',))
+
+    def test_ecsswebusergroup_get_by_natural_key(self):
+        group = Group.objects.create(name='test_group')
+        ecsswebusergroup = EcsswebUserGroup.objects.create(group=group, is_saml=False)
+        self.assertEqual(EcsswebUserGroup.objects.get_by_natural_key('test_group'), ecsswebusergroup)
+
+    def test_samluser_str(self):
+        user = User.objects.create(username='test01', email='test01@example.com', last_name='givenname01', first_name='surname01')
+        samluser = SamlUser.objects.create(user=user, is_persistent=True)
+        self.assertEqual(str(samluser), 'test01')
+
+    def test_samluser_natural_key(self):
+        user = User.objects.create(username='test01', email='test01@example.com', last_name='givenname01', first_name='surname01')
+        samluser = SamlUser.objects.create(user=user, is_persistent=True)
+        self.assertEqual(samluser.natural_key(), ('test01',))
+
+    def test_samluser_get_by_natural_key(self):
+        user = User.objects.create(username='test01', email='test01@example.com', last_name='givenname01', first_name='surname01')
+        samluser = SamlUser.objects.create(user=user, is_persistent=True)
+        self.assertEqual(SamlUser.objects.get_by_natural_key('test01'), samluser)
