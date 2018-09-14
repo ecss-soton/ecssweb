e639e397bd1aca2c05abad94e51e8da7e880a066
Jinxuan Cui <i@cjxol.com>
2018-09-14 23:52:23 +0100

    Fix coding issues

--- fbevents/management/commands/syncupcomingfbevents.py
+++ fbevents/management/commands/syncupcomingfbevents.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from fbevents.utils import sync_upcoming_events_with_fb
 
 class Command(BaseCommand):
-    
+
     help = 'Sync upcoming events information with Facebook page events'
 
     def handle(self, *args, **options):
