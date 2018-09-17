338676e128d696675350533ebb481c49d9a09997
Jinxuan Cui <i@cjxol.com>
2018-09-17 09:16:22 +0100

    New rule for upload filename

--- jumpstart/forms.py
+++ jumpstart/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 
 from .models import Helper
 
@@ -14,3 +15,13 @@ class HelperEditProfileForm(ModelForm):
             self.fields[field].widget.attrs.update({
                 'class': 'form-control',
             })
+
+    # Check photo filesize
+    def clean_photo(self):
+        photo = self.cleaned_data.get('photo', False)
+        if photo:
+            if photo._size > 4*1024*1024:
+                raise ValidationError("Photo file size too large. Supports file up to 4MB.")
+            return photo
+        else:
+            raise ValidationError("Couldn't read uploaded image.")
--- jumpstart/models.py
+++ jumpstart/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+import os
+import uuid
+from .validators import validate_photo_file_extension
 
+
+def photo_file_name(instance, filename):
+    return ('jumpstart2018/helpers/{}-{}{}'.format(instance.username, uuid.uuid4(), os.path.splitext(filename)[1]))
 
 class Group(models.Model):
     name = models.CharField(max_length=20)
@@ -17,5 +23,5 @@ class Helper(models.Model):
     username = models.CharField(max_length=20, primary_key=True)
     name = models.CharField(max_length=50)
     nickname = models.CharField(max_length=50, null=True, blank=True)
-    photo = models.ImageField(upload_to='jumpstart2018/helpers/')
+    photo = models.ImageField(upload_to=photo_file_name, validators=[validate_photo_file_extension])
     group = models.OneToOneField(Group, on_delete=models.SET_NULL, null=True)
