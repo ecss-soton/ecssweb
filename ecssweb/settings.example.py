d197ddb6909a66263328f425feec0ba89cdd4ad9
Jinxuan Cui <i@cjxol.com>
2018-10-06 15:25:40 +0100

    Add Django admin into installed apps

--- ecssweb/settings.example.py
+++ ecssweb/settings.example.py
@@ -38,6 +38,7 @@ CSRF_COOKIE_SECURE = False
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
