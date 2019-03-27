32e89b7c73e6d6fe1ba55dd7d506dc617a3c48f1
Jinxuan Cui <i@cjxol.com>
2019-03-27 17:27:29 +0000

    Add results page

--- election/urls.py
+++ election/urls.py
@@ -6,6 +6,7 @@ app_name='election'
 
 urlpatterns = [
     path('', views.elections, name='elections'),
+    path('results/', views.results, name='results'),
     re_path(r'^(?P<election>[\w-]+)/$', views.election, name='election'),
     re_path(r'^(?P<election>[\w-]+)/(?P<position>[\w-]+)/$', views.PositionView.as_view(), name='position'),
     re_path(r'^(?P<election>[\w-]+)/(?P<position>[\w-]+)/nominate/$', views.NominationView.as_view(), name='nomination'),
